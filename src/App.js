e7b469cdf5a2e9958189dab1ea052f31b00f9455
kyle006 <117977568+kyle006@users.noreply.github.com>
2023-12-01 11:06:12 +0100

    BMI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const BmiCalculator = () => {
+  const [weight, setWeight] = useState('');
+  const [height, setHeight] = useState('');
+  const [gender, setGender] = useState('mänlich');
+  const [bmiResult, setBmiResult] = useState(null);
+
+  const calculateBMI = () => {
+    if (!weight || !height) {
+      alert('Bitte füllen sie alle Felder aus');
+      return;
+    }
+
+    const heightInMeters = height / 100;
+    const bmi = weight / (heightInMeters * heightInMeters);
+
+   
+
+    setBmiResult(bmi.toFixed(2));
+  };
+
+  const getBmiCategory = () => {
+    if (bmiResult < 18.5) {
+      return 'Untergewicht';
+    } else if (bmiResult >= 18.5 && bmiResult <= 24.9) {
+      return 'Normal gewicht';
+    } else if (bmiResult >= 25.0 && bmiResult <= 29.9) {
+      return 'Präadipositas';
+    } else if (bmiResult >= 30.0 && bmiResult <= 34.9) {
+      return 'Adipositas Grad I';
+    } else if (bmiResult >= 35.0 && bmiResult <= 39.9) {
+      return 'Adipositas Grad II';
+    } else {
+      return 'Adipositas Grad III';
+    }
+  };
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1>BMI Calculator</h1>
+      <form>
+        <div >
+          <label >Gewicht (kg)</label>
+          <input value={weight} onChange={(e) => setWeight(e.target.value)} />
+        </div>
+        <div>
+          <label >Grösse (cm)</label>
+          <input value={height} onChange={(e) => setHeight(e.target.value)} />
+        </div>
+        <div >
+          <label>Geschlecht</label>
+          <select  value={gender} onChange={(e) => setGender(e.target.value)}>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+          </select>
+        </div>
+        <button type="button" onClick={calculateBMI}>BMI Ausrechnen</button>
+      </form>
+      {bmiResult !== null && (
+        <div className="mt-4">
+          <h2>Deine BMI: {bmiResult}</h2>
+          <p>BMI Kategorie: {getBmiCategory()}</p>
+        </div>
+      )}
     </div>
   );
-}
+};
 
-export default App;
+export default BmiCalculator;
